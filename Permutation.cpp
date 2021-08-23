7ead3e519a471b2c4873354a80271610b9b66145
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-08-23 10:09:05 +0530

    Permustation prebuilt

---
+++ Permutation.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+vector<vector<int>> ans;
+
+void permute(vector<int> &a, int idx){
+    if(idx == a.size()){
+        ans.push_back(a);
+        return;
+    }
+    for(int i = idx;i<a.size();i++){
+        swap(a[i],a[idx]);
+        permute(a,idx+1);
+        swap(a[i],a[idx]);
+    }
+    return;
+}
+
+int main (){
+
+   int n;
+   cin>>n;
+   vector<int> a(n);
+   for(auto &i : a){
+       cin>>i;
+   }
+//    permute(a,0); when used for recursion method
+
+// prebuilt function
+
+    do{
+        ans.push_back(a);
+    }while(next_permutation(a.begin(), a.end()));
+   for(auto v : ans){
+       for(auto i : v){
+           cout<<i<<" ";
+       }
+       cout<<"\n";
+   }
+
+} 
