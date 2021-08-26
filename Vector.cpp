a49334e46a7ecdf6e7e55065462de93fcdbaf036
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-08-26 11:56:49 +0530

    Vectors added

---
+++ Vector.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+int main (){
+
+   vector<int> v;
+
+   v.push_back(1);
+   v.push_back(2);
+   v.push_back(3);
+
+   // To iterate
+
+   for (int i = 0; i < v.size(); i++)
+   {
+       cout<<v[i]<<endl;
+   }
+   
+   vector<int>::iterator it;
+   for(it=v.begin();it !=v.end();it++){
+       cout<<*it<<endl;
+   }
+
+   for(auto element:v){
+       cout<<element<<endl;
+   }
+
+   v.pop_back(); // 1,2
+
+
+  return 0;
+
+} 
