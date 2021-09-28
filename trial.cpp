c3e064a908f4516df7880b4b3b0177bc4cc80750
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-09-28 23:10:10 +0530

    BST

---
+++ trial.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    vector<int> dict(256,-1); ;
+    for(int i=0;i<s.size();i++){
+        cout<<s[i];
+    }
+
+    return 0;
+}
